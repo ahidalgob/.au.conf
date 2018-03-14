f38c3ad2667385b58b26fc4bcc2f931baad81e31
ahidalgob <ahidalgo94@gmail.com>
2018-03-14 06:15:34 -0400

    Initial dummy script

---
+++ script1.sh
@@ -0,0 +1,110 @@
+########## XFCE
+# appearance:
+# Style: Adwaita-xfce dark
+# icons: numix circle
+# Window manager:
+# Style theme: Numix
+
+# numix circle icons
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-icon-theme-circle
+
+
+# vim
+sudo apt install vim
+sudo apt install vim-gtk
+sudo apt install curl
+sudo apt install xclip
+curl -fsSL https://raw.github.com/cknadler/vim-anywhere/master/install | bash
+
+
+# guake
+sudo apt install guake
+
+# git
+sudo apt install git
+
+# vlc
+# check if light
+sudo apt install vlc
+
+
+# mpv
+sudo add-apt-repository ppa:mc3man/mpv-tests
+sudo apt update
+sudo apt install mpv
+
+# check if light
+sudo apt install gparted
+
+# check if mousepad is installed
+sudo apt remove mousepad
+
+# check if light
+sudo apt install python-pip
+
+# check if light
+sudo apt install virtualenv
+
+# check if light
+sudo apt install haskell-platform
+
+# check if light
+sudo apt install banshee
+
+# check if light
+sudo apt install youtube-dl
+
+
+# check if light
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo apt update
+sudo apt install wine1.8 winetricks
+# open wine a couple of times for it to configure
+
+
+sudo apt install g++
+
+sudo apt install default-jre
+sudo apt install icedtea-plugin
+
+
+
+
+# check if light
+LaTeX
+apt install texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-fonts-recommended
+apt install texstudio
+apt install texlive-lang-spanish
+
+
+
+
+
+
+
+
+
+
+
+
+## Download and install chrome .deb
+## Download and install telegram
+
+
+-- spotify
+-- megatools
+
+
+# Download and install dropbox .deb (o sudo apt install nautilus-dropbox)
+# May need to change (or add) custom command on
+# session and startup>application autostart:
+# /bin/bash -c "sleep 15 && dropbox stop && env DBUS_SESSION_BUS_ADDRESS="" dropbox start -i"
+
+# -- thunar dropbox extension
+# sudo add-apt-repository ppa:xubuntu-dev/extras
+# sudo apt update
+# sudo apt install thunar-dropbox-plugin
+
+
