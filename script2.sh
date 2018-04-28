08186d25d3407b4a13f89758e9e6466c11b7d52c
ahidalgob <ahidalgo94@gmail.com>
2018-04-27 22:24:47 -0400

    script2

---
+++ script2.sh
@@ -0,0 +1,25 @@
+sudo add-apt-repository ppa:numix/ppa
+sudo apt update
+sudo apt install software-properties-common python-software-properties vim vim-gtk curl xclip guake terminator g++ icedtea-plugin default-jre git xfce4-whiskermenu-plugin numix-icon-theme-circle libxslt1.1 gnome-themes-standard python-dev libxml2-dev libxslt-dev build-essential cmake python-dev libxml2-dev libxslt-dev
+
+sudo locale-gen "en_US.UTF-8"
+sudo dpkg-reconfigure locales
+
+
+# link .vimrc .git and add to .bashrc
+
+# download debs: dropbox, chrome, mega
+
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.py --clang-completer
+
+
+
+########## XFCE
+# appearance:
+# Style: Adwaita-xfce dark
+# icons: numix circle
+# Window manager:
+# Style theme: Numix
