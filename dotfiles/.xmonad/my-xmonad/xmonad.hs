ca2b4b33740642135be3dda32c670c9e1a910d55
ahidalgob <ahidalgo94@gmail.com>
2019-10-14 09:45:11 -0400

    polybar working with multiPP

--- dotfiles/.xmonad/my-xmonad/xmonad.hs
+++ dotfiles/.xmonad/my-xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 --import XMonad.Util.ExtensibleState as XS
 import XMonad.Actions.CycleWS
+import XMonad.Hooks.DynamicBars
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks
@@ -22,7 +23,9 @@ import XMonad.Util.NamedWindows (getName)
 import XMonad.Util.Run (spawnPipe, safeSpawn)
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
+import System.Posix.Files
 import System.Exit
+import           System.Process                 ( callCommand )
 
 import qualified XMonad.StackSet as W
 
@@ -36,75 +39,51 @@ import Data.Monoid((<>))
 myblack1  = "#151515"
 myblack2  = "#212121"
 myblack3  = "#505050"
-myblue2   = "#6c99bb"
+myblue2   = "#7ca9cb"
+myblue3   = "#3c698b"
 mymagenta = "#9f4e85"
 mywhite0  = "#a0a0a0"
 mywhite1  = "#d0d0d0"
 
-myCurrentWS = mymagenta
-myOtherScreenWS = myblack3
-myActiveWS = myblack2
+myCurrentWS = myblue2
+myOtherScreenWS = myblue3
+hiddenWS = myblack3
+-- myActiveWS = myblack2
 
 myLogHook :: X ()
 myLogHook = do
   winset <- gets windowset
   title <- maybe (return "") (fmap show . getName) . W.peek $ winset
-  let currWs = W.currentTag winset
-  let notEmptyWss = [W.tag w | w <- W.workspaces winset, (isJust . W.stack) w]
-  let wss = filter (\w -> w `elem` notEmptyWss || w == currWs) myWorkspaces
-  let wsStr = join $ map (fmt currWs) wss
+  multiPP pP' pP'
+  spawn $ "echo \"" ++ (take 50 title ++ "\n") ++ "\" >> /tmp/.xmonad-title-log"
+  spawn $ "echo \"" ++ (take 50 title ++ "\n") ++ "\" >> /tmp/.xmonad-title-hdmi-log"
 
-  io $ appendFile "/tmp/.xmonad-title-log" (title ++ "\n")
-  io $ appendFile "/tmp/.xmonad-workspace-log" (wsStr ++ "\n")
+underline color s = "%{u" ++ color ++ "}%{+u}" ++ s ++ "%{-u}"
+overline color s = "%{o" ++ color ++ "}%{+o}" ++ s ++ "%{-o}"
 
-  where fmt currWs ws
-          | currWs == ws =  underline myblue2 $ clickableWS ws
-          | otherwise    = clickableWS ws
-        underline color s = "%{u" ++ color ++ "}%{+u}" ++ s ++ "%{-u}"
-        overline color s = "%{o" ++ color ++ "}%{+o}" ++ s ++ "%{-o}"
-        clickableWS ws = " %{A1:xdotool key alt+" ++ [head ws] ++ ":}" ++ ws ++ "%{A} "
+pP' :: PP
+pP' = tryPP undefined
 
-{-
-tryPP :: (Handle, ScreenId) -> X PP
-tryPP (h, sid) = do
-  Just wid <- screenWorkspace sid
-  return $ def
-    { ppOutput    = hPutStrLn h
-    , ppCurrent = visibleWS wid
-    , ppVisible = visibleWS wid
-    , ppHidden    = dzenColor mywhite1 myActiveWS . clickableWS . pad
+tryPP :: Handle -> PP
+tryPP h = def
+    { ppOutput  = hPutStrLn h
+    , ppCurrent = pad . underline myCurrentWS . clickableWS
+    , ppVisible = pad . underline myOtherScreenWS . clickableWS
+    , ppHidden  = pad . clickableWS
+    , ppHiddenNoWindows = pad . foreGround hiddenWS . clickableWS
     , ppWsSep   = ""
     , ppSep     = ""
-
-    , ppTitle   = wrap vtitle vtitleEnd . shorten 60
-    , ppLayout    = dzenColor mywhite1 myblack1 . pad .
-      (\t -> calIc ++ (case last . words $ t of
-              "Tall"  -> "sptall.xbm"
-              "Full"  -> "full.xbm"
-              "Grid" -> "grid.xbm"
-              _      -> "grid.xbm" -- Need this because of WS 0
-        ) ++  ")^ca()")
+    , ppTitle   = shorten 60
+    , ppLayout  = const " "
     , ppOrder = \(ws:l:t:_) ->
-        [ l
-        , clickable "5" "alt+Page_Down" $ clickable "4" "alt+Page_Up" ws
-        , dzenColor mywhite0 mywhite0 " "
-        , t]
+        [ scrollableWS ws ]
     }
   where
-  visibleWS wid ws = if wid == ws
-    then dzenColor mywhite1 myCurrentWS . clickableWS . pad $ ws
-    else dzenColor mywhite1 myOtherScreenWS . clickableWS . pad $ ws
-  calIc = "^ca(1,xdotool key alt+space)^i(/home/augusto/.xmonad/icons/"
-  vtitle = "^bg(" ++ myblack3 ++ ")  "
-  vtitleEnd = "  ^bg()"
-  clickable mouseKey action string =
-    "^ca("++mouseKey++",xdotool key "++action++")" ++ string ++ "^ca()"
-  -- TODO this wont work with - or =, need to check if xdotool understand the
-  -- first character (for - is xK_minus and so on)
-  clickableWS ws = "^ca(1,xdotool key alt+" ++ [ws!!1] ++ ")" ++ ws ++ "^ca()" -- 2nd because of padding space
-
--}
-
+  foreGround color s = "%{F" ++ color ++ "}" ++ s ++ "%{F-}"
+  clickableWS ws = "%{A1:xdotool key alt+" ++ [head ws] ++ ":}" ++ ws ++ "%{A}"
+  scrollableWS wss = "%{A4:xdotool key super+k:}" ++
+                     "%{A5:xdotool key super+j:}" ++
+                     wss ++ "%{A}%{A}"
 
 myExtraWS = [("0", xK_0),("-", xK_minus),("=", xK_equal)]
 
@@ -119,12 +98,13 @@ myXPConfig = def
   , font              = "xft:fira code:size=12"
   }
 
+superMask = mod4Mask
 myKeys = [ ((mod1Mask .|. shiftMask, xK_q), confirmPrompt myXPConfig "exit" $ io exitSuccess)
-         , ((mod1Mask, xK_p), spawn "rofi -combi-modi window,drun,run -show combi -modi combi,drun -font 'InputMono 10' -show-icons -theme solarized") -- case insensitive
+         , ((mod1Mask, xK_p), spawn "rofi -m -4 -combi-modi window,drun,run -show combi -modi combi,drun -font 'InputMono 10' -show-icons -theme solarized") -- case insensitive
          , ( (mod1Mask, xK_f)
            , withFocused $ windows . flip W.float (W.RationalRect 0 0 1 1))
          , ( (mod1Mask, xK_q)
-           , spawn $ "killall polybar; sleep 1;"
+           , spawn $ "killall polybar; sleep 1"
              <> "xmonad --recompile; xmonad --restart")
 
          , ((0, xK_Print), spawn "scrot")
@@ -132,14 +112,16 @@ myKeys = [ ((mod1Mask .|. shiftMask, xK_q), confirmPrompt myXPConfig "exit" $ io
          , ((shiftMask, xK_Print), spawn "sleep 0.3; scrot -s")
          --, ((controlMask, xK_Print), spawn "gnome-screenshot --area")
 
-         , ((mod1Mask, xK_Page_Up), moveTo Prev NonEmptyWS)
-         , ((mod1Mask, xK_Page_Down), moveTo Next NonEmptyWS)
-         , ((mod1Mask .|. shiftMask, xK_Page_Up), prevWS)
-         , ((mod1Mask .|. shiftMask, xK_Page_Down), nextWS)
+         , ((superMask, xK_k), moveTo Prev NonEmptyWS)
+         , ((superMask, xK_j), moveTo Next NonEmptyWS)
+         , ((superMask .|. shiftMask, xK_k), prevWS)
+         , ((superMask .|. shiftMask, xK_j), nextWS)
+
+         , ((mod1Mask .|. shiftMask, xK_b), toggleScreenSpacingEnabled >> toggleWindowSpacingEnabled)
 
          --, ((mod1Mask .|. shiftMask, xK_space), sendMessage PrevLayout) TODO Is this impossible?
          --
-         , ((controlMask .|. shiftMask, xK_space), spawn "~/.au.conf/scripts/toogleKeyboardLayout")
+         , ((controlMask .|. shiftMask, xK_space), spawn "~/.au.conf/scripts/toggleKeyboardLayout")
 
          , ((0, xF86XK_AudioLowerVolume ), spawn "amixer set Master 2%-")
          , ((0, xF86XK_AudioRaiseVolume ), spawn "amixer set Master 2%+")
@@ -150,10 +132,10 @@ myKeys = [ ((mod1Mask .|. shiftMask, xK_q), confirmPrompt myXPConfig "exit" $ io
          ++
          [ ((mod1Mask .|. shiftMask, key), windows $ W.shift ws) | (ws, key) <- myExtraWS ]
 
-myLayout = avoidStruts $
+myLayout = lessBorders OnlyScreenFloat $ avoidStruts $
     spacingRaw False myBorder True myBorderIn True $
     onWorkspace "0" Grid $
-    reflectHoriz $ withIM (67%256) (ClassName "TelegramDesktop") $ reflectHoriz
+    reflectHoriz $ withIM (71%256) (ClassName "TelegramDesktop") $ reflectHoriz
     (sTall ||| Full)
     where
       sTall = Tall 1 (2/100) (1/2)
@@ -182,23 +164,31 @@ myApps = composeAll $
       , "Toggl Desktop"
       ]
 
-main = do
-    spawn "polybar example"
-    safeSpawn "mkfifo" ["/tmp/" ++ ".xmonad-workspace-log"]
-    safeSpawn "mkfifo" ["/tmp/" ++ ".xmonad-title-log"]
+barInScreen :: ScreenId -> IO Handle
+barInScreen (S 0) = do
+    spawn "sleep 1; polybar bar1"
+    spawnPipe "cat >> /tmp/.xmonad-workspace-log"
+barInScreen (S 1) = do
+    spawn "sleep 1; polybar barhdmi"
+    spawnPipe "cat >> /tmp/.xmonad-workspace-hdmi-log"
 
+main = do
+    spawn "beep -f 165.4064 -l 100 -n -f 130.813 -l 100 -n -f 261.626 -l 100 -n -f 523.251 -l 100 -n -f1046.50 -l 100 -n -f 2093.00 -l 100 -n -f 4186.01 -l 200"
     xmonad $ ewmh def
       { manageHook = myApps <+> manageDocks <+> manageHook def
       , layoutHook = myLayout
-      , handleEventHook = docksEventHook <+> handleEventHook def
+      , handleEventHook = docksEventHook
+                        <+> dynStatusBarEventHook barInScreen (spawn "killall polybar")
+                        <+> handleEventHook def
       , modMask = mod1Mask
       , workspaces = myWorkspaces
       , terminal  = "urxvt -e tmux"
       , focusedBorderColor = myblue2
       , normalBorderColor = "" ++ myblack2 ++ ""
-      , borderWidth = 3
+      , borderWidth = 2
       , startupHook = setWMName "LG3D"
                     <+> docksStartupHook
+                    <+> dynStatusBarStartup barInScreen (return ())
       , logHook = fadeInactiveLogHook 0.9
-                  <+> myLogHook
+                <+> myLogHook
       } `additionalKeys` myKeys
